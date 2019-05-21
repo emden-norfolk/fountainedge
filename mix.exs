79431551cf6a21abfe9c6625a19cfb0808fc90e7
Damien Bezborodow <dbezborodov@gmail.com>
2019-05-21 12:08:54 +0930

    Mix.

--- mix.exs
+++ mix.exs
@@ -8,7 +8,9 @@ defmodule Fountainedge.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      package: package(),
+      deps: deps(),
       source_url: "https://github.com/emden-norfolk/fountainedge"
     ]
   end
@@ -25,6 +27,22 @@ defmodule Fountainedge.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  defp description() do
+    "Workflow engine with conditions, forks and parallel processes."
+  end
+
+  defp package() do
+    [
+      # This option is only needed when you don't want to use the OTP application name
+      name: "fountainedge",
+      # These are the default files included in the package
+      files: ~w(lib priv mix.exs README* readme* LICENSE*
+                license* CHANGELOG* changelog* src),
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/emden-norfolk/fountainedge"}
     ]
   end
 end
