2fc71a4bd52cc58f5cf3d66a91c90e10db65f654
Damien Bezborodow <dbezborodov@gmail.com>
2019-05-21 11:50:28 +0930

    New package.

---
+++ lib/fountainedge.ex
@@ -0,0 +1,18 @@
+defmodule Fountainedge do
+  @moduledoc """
+  Documentation for Fountainedge.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Fountainedge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/fountainedge_test.exs
@@ -0,0 +1,8 @@
+defmodule FountainedgeTest do
+  use ExUnit.Case
+  doctest Fountainedge
+
+  test "greets the world" do
+    assert Fountainedge.hello() == :world
+  end
+end
