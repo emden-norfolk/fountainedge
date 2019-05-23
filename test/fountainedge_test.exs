680e7df4662a14c4478ab61a4399b500a0e501f0
Damien Bezborodov <dbezborodov@gmail.com>
2019-05-23 15:13:50 +0930

    Assertions.

--- test/fountainedge_test.exs
+++ test/fountainedge_test.exs
@@ -22,7 +22,29 @@ defmodule FountainedgeTest do
     }
 
     workflow = Workflow.transition(workflow, %Edge{id: 1, next: 2})
+    assert workflow.states == [%State{id: 2}]
+
     workflow = Workflow.transition(workflow, %Edge{id: 2, next: 3})
-    IO.inspect workflow
+    assert workflow.states == [%State{id: 3}]
+  end
+
+  test "can compute forks and joins" do
+    workflow = %Workflow{
+      nodes: [
+        %Node{id: 1, type: 1},
+        %Node{id: 2},
+        %Node{id: 3, type: 2},
+      ],
+      edges: [
+        %Edge{id: 1, next: 2},
+        %Edge{id: 2, next: 3},
+      ],
+      states: [
+        %State{id: 1},
+      ],
+    }
+
+    workflow = Workflow.transition(workflow, %Edge{id: 1, next: 2})
+    workflow = Workflow.transition(workflow, %Edge{id: 2, next: 3})
   end
 end
