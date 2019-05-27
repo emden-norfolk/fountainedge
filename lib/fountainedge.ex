c0b985afffe7dc267e6dcce0181eda65c57b5675
Damien Bezborodov <dbezborodov@gmail.com>
2019-05-27 16:38:06 +0930

    No idea!

--- lib/fountainedge.ex
+++ lib/fountainedge.ex
@@ -7,21 +7,17 @@ defmodule Fountainedge do
   alias Fountainedge.{Schema, Edge, State, Node, Token}
 
   @fork Node.fork
-  @join Node.join
+  #@join Node.join
 
   defstruct schema: %Schema{}, states: []
 
   def transition %Workflow{} = workflow, %Edge{} = edge do
     #edge = Enum.find workflow.edges, fn e -> e == edge end
 
-    %Workflow{workflow | states: transition_state(workflow.states, workflow.schema, edge)}
+    %Workflow{workflow | states: move(workflow.states, workflow.schema, edge)}
   end
 
-  defp advance states, schema do
-    states
-  end
-
-  defp transition_state states, %Schema{} = schema, %Edge{} = edge do
+  defp move states, %Schema{} = schema, %Edge{} = edge do
     state = Enum.find states, fn s -> s.id == edge.id end
     states = Enum.reject states, fn s -> s == state end
     next_state = %State{state | id: edge.next}
@@ -38,20 +34,28 @@ defmodule Fountainedge do
     states
   end
 
+  def move states, %Schema{} = schema, edges do
+    states = move
+  end
+
   defp fork states, %Schema{} = schema, %Node{} = node, %State{} = next_state do
     edges = Enum.filter schema.edges, fn e -> e.id == node.id end
     forked_states = Enum.reduce edges, [], fn edge, acc ->
       token = %Token{id: edge.id, token: edge.next}
       tokens = next_state.tokens ++ [token]
       [%State{next_state | tokens: tokens} | acc]
-      |> advance(schema)
-      |> Enum.reverse
     end
+
     states = Enum.reject states, fn s -> s.id == next_state.id end
     states ++ forked_states
+
+    Enum.filter
+
+    Enum.reduce forked_states, [], fn state, acc ->
+      edge = Enum.find(schema.edges, fn e -> e.id == state.id end)
+      [state | acc]
+    end
   end
 
-  defp join workflow do
-    workflow
-  end
+  #defp join workflow do
 end
