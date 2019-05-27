7619086d9e4473c42e7fa97cb9cff08c0d74e42f
Damien Bezborodov <dbezborodov@gmail.com>
2019-05-27 12:08:55 +0930

    Fork WIP.

--- lib/fountainedge.ex
+++ lib/fountainedge.ex
@@ -4,19 +4,27 @@ defmodule Fountainedge do
   """
 
   alias __MODULE__, as: Workflow
-  alias Fountainedge.{Edge, State}
+  alias Fountainedge.{Edge, State, Node}
 
   defstruct nodes: [], edges: [], states: []
 
-  def transition(workflow, edge) do
-    edge = Enum.find(workflow.edges, fn e -> e == edge end)
-    state  = Enum.find(workflow.states, fn s -> s.id == edge.id end)
+  def transition workflow, edge do
+    edge = Enum.find workflow.edges, fn e -> e == edge end
+    state = Enum.find workflow.states, fn s -> s.id == edge.id end
 
-    Workflow.update_state(workflow, edge, state)
+    transition_state workflow, edge, state
   end
 
-  def update_state(%Workflow{} = workflow, %Edge{} = edge, %State{} = state) do
-    states = Enum.reject(workflow.states, fn s -> s == state end)
-    %Workflow{workflow | states: states ++ [%State{state | id: edge.next}]}
+  defp transition_state %Workflow{} = workflow, %Edge{} = edge, %State{} = state do
+    states = Enum.reject workflow.states, fn s -> s == state end
+    next_state = %State{state | id: edge.next}
+    workflow = %Workflow{workflow | states: states ++ [next_state]}
+
+    node = Enum.find workflow.nodes, fn n -> n.id == edge.next end
+    if node.type == Node.fork do
+      Enum.filter workflow.edges, fn e -> e.id == node.id end
+    end
+
+    workflow
   end
 end
