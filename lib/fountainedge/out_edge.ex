1572aa42c77d4eb2d3236194b6c96d40d4a2555f
Damien Bezborodow <dbezborodov@gmail.com>
2023-01-22 14:25:10 +1030

    Deprecate OutEdge?

--- lib/fountainedge/out_edge.ex
+++ lib/fountainedge/out_edge.ex
@@ -5,5 +5,9 @@ defmodule Fountainedge.OutEdge do
 
   @enforce_keys [:edge]
 
+  # TODO remove this class entirely?
+  # How useful is a disabled out edge?
+  # Could be more confusing than anything.
+  # Check client usage. Deprecate in version 1?
   defstruct edge: nil, disabled: false
 end
